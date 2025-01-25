065b544ab949a71c76f99abab43c9d9e89679945
Daniel Baptista <bapniel21@gmail.com>
2025-01-25 14:37:19 -0300

    Create index.js

---
+++ aula05/index.js
@@ -0,0 +1,16 @@
+//Strings
+const nome = "Daniel";
+const endereco = "Rua das Flores, 123";
+console.log(typeof nome, typeof endereco);
+
+//Number
+const numero = 123;
+console.log(typeof numero);
+
+//Boolean
+const isCadastrado = true;
+
+console.log(typeof isCadastrado);
+
+let sobrenome;
+console.log(typeof sobrenome);
